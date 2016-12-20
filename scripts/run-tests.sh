934f434725e1c71d845c19241b91f7879795ef6e
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-12-20 17:11:31 +0200

    Implement arabic translations

---
+++ scripts/run-tests.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -e
+
+echo 'Beginning Test Run...'
+echo ''
+
+echo 'Removing *.pyc files'
+find . -name "*.pyc" -exec rm -rf {} \;
+
+echo 'Running test suite'
+coverage run manage.py test edx_proctoring --verbosity=3
+
+echo 'Building coverage reports'
+coverage report -m
+coverage html
+echo ''
+echo 'View the full coverage report at {CODE_PATH}/edx-proctoring/htmlcov/index.html'
+
+echo 'Checking styling'
+pep8 edx_proctoring
+pylint edx_proctoring --report=no
+echo 'Styling check complete'
+
+echo ''
+echo 'Testing Complete!'
